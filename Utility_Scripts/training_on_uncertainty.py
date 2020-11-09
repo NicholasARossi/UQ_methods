a7acb9334d9370c98897e4bcabfc5964cf4f6adc
Nicholas Rossi <nicholas.rossi@synthego.com>
2020-11-08 20:24:58 -0800

    point detection updates

--- Utility_Scripts/training_on_uncertainty.py
+++ Utility_Scripts/training_on_uncertainty.py
@@ -1,3 +1,26 @@
+from sklearn.decomposition import PCA
+
+import numpy as np
 
 
-def multi_dimension
+def PCA_binning(data,x_cols=None,y_col=None,n_components=1,n_bins=100):
+    '''
+
+    :param data:
+    :param x_cols:
+    :param y_cols:
+    :return:
+    '''
+    pca = PCA(n_components=n_components)
+    data['PCA_values'] = pca.fit_transform(data[x_cols])
+
+    bins = np.linspace(min(data['PCA_values']), max(data['PCA_values']), n_bins)
+    data['bins'] = np.digitize(data['PCA_values'], bins=bins, right=True)
+
+    reduced_df=data.groupby('bins').mean().rename(columns={y_col:'y_mean'})
+    reduced_df.loc[:,'y_std']=data.groupby('bins').std().rename(columns={y_col:'y_std'})['y_std']
+
+    return reduced_df,data
+
+
+
